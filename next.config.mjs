f9c7e841ab916a9a63de3f8f26f0bb0c75960fde
Phuong Dong <catmusic435@gmail.com>
2026-04-04 00:30:53 +0700

    feat: remove image optimization and implement Content-Security-Policy headers

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,32 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
   compress: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
   experimental: {
     optimizePackageImports: ['lucide-react', 'framer-motion'],
+  }, async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              font-src 'self' https://fonts.gstatic.com;
+              img-src 'self' data: https:;
+              connect-src 'self' https:;
+              frame-src https://www.youtube.com https://www.google.com;
+            `.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ]
   },
 }
 
